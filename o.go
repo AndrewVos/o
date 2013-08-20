f9d41960fa7e10bd212b9d14783b0cabd2d08803
Andrew Vos <andrew.vos@gmail.com>
2013-08-20 21:21:37 +0100

    refactor

--- o.go
+++ o.go
@@ -21,35 +21,31 @@ func OO(i interface{} ) string {
 }
 
 func writeStruct(interfaceValue interface{}, structType reflect.Type) string {
-  s := colouriseStructTitle(structType.Name()) + " {\n"
-
-  attrs := map[string]string {}
+  attributes := map[string]string {}
   for i := 0; i < structType.NumField(); i++ {
     value := reflect.ValueOf(interfaceValue).Field(i)
     field := structType.Field(i)
 
     if !field.Anonymous {
       if value.Kind() == reflect.Int {
-        attrs[field.Name] = strconv.Itoa(int(value.Int()))
+        attributes[field.Name] = strconv.Itoa(int(value.Int()))
       } else {
-        attrs[field.Name] = value.String()
+        attributes[field.Name] = value.String()
       }
     }
   }
 
   widest := 0
-  for name,_ := range attrs {
+  for name,_ := range attributes {
     if length := len(name); length > widest { widest = length }
   }
 
   allFields := []string{}
-  for name, value := range attrs {
+  for name, value := range attributes {
     allFields = append(allFields, "  " + colouriseField(rjust(name, widest)) + ": " + colouriseValue(value))
   }
 
-  s = s + strings.Join(allFields, "\n") + "\n}\n"
-
-  return s
+  return colouriseStructTitle(structType.Name()) + " {\n" + strings.Join(allFields, "\n") + "\n}\n"
 }
 
 func colouriseStructTitle(title string) string { return colour.Blue(title) }
