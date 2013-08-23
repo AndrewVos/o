2f827ac9c567497160535fde90ed4a0cf868e64b
Andrew Vos <andrew.vos@gmail.com>
2013-08-23 22:09:07 +0100

    switch to using tabs :(

--- o.go
+++ o.go
@@ -1,119 +1,119 @@
 package o
 
 import (
-  "fmt"
-  "reflect"
-  "strconv"
-  "github.com/AndrewVos/colour"
+	"fmt"
+	"reflect"
+	"strconv"
+	"github.com/AndrewVos/colour"
 )
 
 func O(thing interface{}) {
-  fmt.Println(o(thing))
+	fmt.Println(o(thing))
 }
 
 func o(thing interface{}) string {
-  return write("", 0, thing)
+	return write("", 0, thing)
 }
 
 func write(name string, depth int, thing interface{}) string {
-  result := name
+	result := name
 
-  thingType := reflect.TypeOf(thing)
-  if thingType.Kind() == reflect.Ptr{ thingType = thingType.Elem() }
+	thingType := reflect.TypeOf(thing)
+	if thingType.Kind() == reflect.Ptr{ thingType = thingType.Elem() }
 
-  if thingType.Kind() == reflect.Int {
-    result += writeInt(thing)
-  } else if thingType.Kind() == reflect.String {
-    result += writeString(thing)
-  } else if thingType.Kind() == reflect.Bool {
-    result += writeBool(thing)
-  } else if thingType.Kind() == reflect.Struct {
-    result += writeStruct(depth, thing)
-  } else if thingType.Kind() == reflect.Slice {
-    result += writeSlice(depth, thing)
-  } else if thingType.Kind() == reflect.Map {
-    result += writeMap(depth, thing)
-  }
-  return result
+	if thingType.Kind() == reflect.Int {
+		result += writeInt(thing)
+	} else if thingType.Kind() == reflect.String {
+		result += writeString(thing)
+	} else if thingType.Kind() == reflect.Bool {
+		result += writeBool(thing)
+	} else if thingType.Kind() == reflect.Struct {
+		result += writeStruct(depth, thing)
+	} else if thingType.Kind() == reflect.Slice {
+		result += writeSlice(depth, thing)
+	} else if thingType.Kind() == reflect.Map {
+		result += writeMap(depth, thing)
+	}
+	return result
 }
 
 func writeBool(thing interface{}) string {
-  thingValue := reflect.ValueOf(thing)
-  if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
-  if thingValue.Bool() {
-    return colourValue("true")
-  } else {
-    return colourValue("false")
-  }
+	thingValue := reflect.ValueOf(thing)
+	if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
+	if thingValue.Bool() {
+		return colourValue("true")
+	} else {
+		return colourValue("false")
+	}
 }
 
 func writeInt(thing interface{}) string {
-  thingValue := reflect.ValueOf(thing)
-  if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
-  return colourValue(strconv.Itoa(int(thingValue.Int())))
+	thingValue := reflect.ValueOf(thing)
+	if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
+	return colourValue(strconv.Itoa(int(thingValue.Int())))
 }
 
 func writeSlice(depth int, thing interface{}) string {
-  result := colourTitle("slice") + " [" + "\n"
-  thingValue := reflect.ValueOf(thing)
-  if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
+	result := colourTitle("slice") + " [" + "\n"
+	thingValue := reflect.ValueOf(thing)
+	if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
 
-  for elementIndex := 0; elementIndex < thingValue.Len(); elementIndex++ {
-    element := thingValue.Index(elementIndex).Interface()
-    result += margin(depth + 1) + write("", depth + 1, element) + ",\n"
-  }
+	for elementIndex := 0; elementIndex < thingValue.Len(); elementIndex++ {
+		element := thingValue.Index(elementIndex).Interface()
+		result += margin(depth + 1) + write("", depth + 1, element) + ",\n"
+	}
 
-  result += margin(depth) + "]"
-  return result
+	result += margin(depth) + "]"
+	return result
 }
 
 func writeString(thing interface{}) string {
-  thingValue := reflect.ValueOf(thing)
-  if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
-  quote := colourQuotes(`"`)
-  return quote + colourValue(thingValue.String()) + quote
+	thingValue := reflect.ValueOf(thing)
+	if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
+	quote := colourQuotes(`"`)
+	return quote + colourValue(thingValue.String()) + quote
 }
 
 func writeStruct(depth int, thing interface{}) string {
-  thingType := reflect.TypeOf(thing)
-  if thingType.Kind() == reflect.Ptr{ thingType = thingType.Elem() }
-  value := reflect.ValueOf(thing)
-  if value.Kind() == reflect.Ptr { value = value.Elem() }
+	thingType := reflect.TypeOf(thing)
+	if thingType.Kind() == reflect.Ptr{ thingType = thingType.Elem() }
+	value := reflect.ValueOf(thing)
+	if value.Kind() == reflect.Ptr { value = value.Elem() }
 
-  result := colourTitle(thingType.Name()) + " {\n"
+	result := colourTitle(thingType.Name()) + " {\n"
 
-  for fieldIndex := 0; fieldIndex < thingType.NumField(); fieldIndex++ {
-    field := thingType.Field(fieldIndex)
+	for fieldIndex := 0; fieldIndex < thingType.NumField(); fieldIndex++ {
+		field := thingType.Field(fieldIndex)
 
-    if !field.Anonymous {
-      childThing := value.Field(fieldIndex).Interface()
-      displayName := colourField(field.Name) + ": "
-      result += margin(depth + 1) + write(displayName, depth + 1, childThing) + "\n"
-    }
-  }
+		if !field.Anonymous {
+			childThing := value.Field(fieldIndex).Interface()
+			displayName := colourField(field.Name) + ": "
+			result += margin(depth + 1) + write(displayName, depth + 1, childThing) + "\n"
+		}
+	}
 
-  return result + margin(depth) + "}"
+	return result + margin(depth) + "}"
 }
 
 func writeMap(depth int, thing interface{}) string {
-  result := colourTitle("map") + " {\n"
+	result := colourTitle("map") + " {\n"
 
-  thingValue := reflect.ValueOf(thing)
-  if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
-  for _, key := range thingValue.MapKeys() {
-    mapValue := thingValue.MapIndex(key)
-    result += margin(depth + 1) + write("", depth + 1, key.Interface()) + ": " + write("", depth + 1, mapValue.Interface()) + ",\n"
-  }
-  return result + margin(depth) + "}"
+	thingValue := reflect.ValueOf(thing)
+	if thingValue.Kind() == reflect.Ptr { thingValue = thingValue.Elem() }
+	for _, key := range thingValue.MapKeys() {
+		mapValue := thingValue.MapIndex(key)
+		result += margin(depth + 1) + write("", depth + 1, key.Interface()) + ": " + write("", depth + 1, mapValue.Interface()) + ",\n"
+	}
+	return result + margin(depth) + "}"
 }
 
 func margin(depth int) string {
-  m := ""
-  if depth == 0 { return m }
-  for i:= 1; i <= depth; i++ {
-    m += "  "
-  }
-  return m
+	m := ""
+	if depth == 0 { return m }
+	for i:= 1; i <= depth; i++ {
+		m += "  "
+	}
+	return m
 }
 
 func colourQuotes(quote string) string { return colour.Red(quote) }
--- o_test.go
+++ o_test.go
@@ -1,113 +1,113 @@
 package o
 
 import (
-  "testing"
-  "strings"
-  "regexp"
-  "os"
-  "fmt"
+	"testing"
+	"strings"
+	"regexp"
+	"os"
+	"fmt"
 )
 
 type SimpleStruct struct {
-  Name string
-  Age int
-  Married bool
+	Name string
+	Age int
+	Married bool
 }
 type StructWithName struct { Name string }
 type StructWithAge struct { Age int }
 type StructWithDepth struct {
-  NameStruct StructWithName
-  AgeStruct StructWithAge
+	NameStruct StructWithName
+	AgeStruct StructWithAge
 }
 type Thing struct { ThingValue string }
 type StructWithSlices struct { Things []Thing }
 type StructWithPointerFields struct { SomeField *int }
 
 func assertOutput(t *testing.T, value interface{}, expected string) {
-  begin := regexp.MustCompile("\\x1b\\[3[1-9];1m")
-  end := regexp.MustCompile("\\x1b\\[0m")
-  actual := o(value)
-  if os.Getenv("OUTPUT") != "" {
-    fmt.Println(actual)
-    fmt.Println("----------------------------------------------------------------------------------------------------")
-  }
-  actual = begin.ReplaceAllString(actual, "")
-  actual = end.ReplaceAllString(actual, "")
-  actual = strings.TrimSpace(actual)
-  expected = strings.TrimSpace(expected)
-  if actual != expected {
-    t.Errorf("Expected:\n%v\nGot:\n%v\n", expected, actual)
-  }
+	begin := regexp.MustCompile("\\x1b\\[3[1-9];1m")
+	end := regexp.MustCompile("\\x1b\\[0m")
+	actual := o(value)
+	if os.Getenv("OUTPUT") != "" {
+		fmt.Println(actual)
+		fmt.Println("----------------------------------------------------------------------------------------------------")
+	}
+	actual = begin.ReplaceAllString(actual, "")
+	actual = end.ReplaceAllString(actual, "")
+	actual = strings.TrimSpace(actual)
+	expected = strings.TrimSpace(expected)
+	if actual != expected {
+		t.Errorf("Expected:\n%v\nGot:\n%v\n", expected, actual)
+	}
 }
 
 func TestString(t *testing.T) {
-  assertOutput(t, "o.OOO", `"o.OOO"`)
+	assertOutput(t, "o.OOO", `"o.OOO"`)
 }
 
 func TestPointerString(t *testing.T) {
-  s := "sdfwef"
-  assertOutput(t, &s, `"sdfwef"`)
+	s := "sdfwef"
+	assertOutput(t, &s, `"sdfwef"`)
 }
 
 func TestInt(t * testing.T) {
-  assertOutput(t, 12345, "12345")
+	assertOutput(t, 12345, "12345")
 }
 
 func TestPointerInt(t * testing.T) {
-  s := 13113
-  assertOutput(t, &s, "13113")
+	s := 13113
+	assertOutput(t, &s, "13113")
 }
 
 func TestBool(t *testing.T) {
-  assertOutput(t, true, "true")
-  assertOutput(t, false, "false")
+	assertOutput(t, true, "true")
+	assertOutput(t, false, "false")
 }
 
 func TestPointerBool(t *testing.T) {
-  s := true
-  assertOutput(t, &s, "true")
-  s = false
-  assertOutput(t, &s, "false")
+	s := true
+	assertOutput(t, &s, "true")
+	s = false
+	assertOutput(t, &s, "false")
 }
 
 func TestStruct(t *testing.T) {
-  s := SimpleStruct{Name: "Arthur", Age: 42}
-  expected := `
+	s := SimpleStruct{Name: "Arthur", Age: 42}
+	expected := `
 SimpleStruct {
   Name: "Arthur"
   Age: 42
   Married: false
 }
-  `
-  assertOutput(t, s, expected)
+	`
+	assertOutput(t, s, expected)
 }
 
 func TestPointerStruct(t *testing.T) {
-  s := SimpleStruct{Name: "Hello"}
-  expected := `
+	s := SimpleStruct{Name: "Hello"}
+	expected := `
 SimpleStruct {
   Name: "Hello"
   Age: 0
   Married: false
 }
-  `
-  assertOutput(t, &s, expected)
+	`
+	assertOutput(t, &s, expected)
 }
 
 func TestStructWithPointerFields(t *testing.T) {
-  i := 1000
-  s := StructWithPointerFields{SomeField: &i}
-  expected := `
+	i := 1000
+	s := StructWithPointerFields{SomeField: &i}
+	expected := `
 StructWithPointerFields {
   SomeField: 1000
 }
-  `
-  assertOutput(t, &s, expected)
+	`
+	assertOutput(t, &s, expected)
 }
 
 func TestStructWithDepth(t *testing.T) {
-  s := StructWithDepth { NameStruct: StructWithName { Name: "Mika" }, AgeStruct: StructWithAge { Age: 10 } }
-  expected := `
+	s := StructWithDepth { NameStruct: StructWithName { Name: "Mika" }, AgeStruct: StructWithAge { Age: 10 } }
+	expected := `
 StructWithDepth {
   NameStruct: StructWithName {
     Name: "Mika"
@@ -116,36 +116,36 @@ StructWithDepth {
     Age: 10
   }
 }
-  `
-  assertOutput(t, s, expected)
+	`
+	assertOutput(t, s, expected)
 }
 
 func TestSlice(t *testing.T) {
-  s := []Thing { { ThingValue: "ererrrmmmm" }, }
-  expected := `
+	s := []Thing { { ThingValue: "ererrrmmmm" }, }
+	expected := `
 slice [
   Thing {
     ThingValue: "ererrrmmmm"
   },
 ]
-  `
-  assertOutput(t, s, expected)
+	`
+	assertOutput(t, s, expected)
 }
 
 func TestPointerSlice(t *testing.T) {
-  a := "sdfsd"
-  s := []*string { &a }
-  expected := `
+	a := "sdfsd"
+	s := []*string { &a }
+	expected := `
 slice [
   "sdfsd",
 ]
-  `
-  assertOutput(t, &s, expected)
+	`
+	assertOutput(t, &s, expected)
 }
 
 func TestStructWithSlices(t *testing.T) {
-  s := StructWithSlices{ Things: []Thing{ { ThingValue: "ermmm" }, } }
-  expected := `
+	s := StructWithSlices{ Things: []Thing{ { ThingValue: "ermmm" }, } }
+	expected := `
 StructWithSlices {
   Things: slice [
     Thing {
@@ -153,40 +153,40 @@ StructWithSlices {
     },
   ]
 }
-  `
-  assertOutput(t, s, expected)
+	`
+	assertOutput(t, s, expected)
 }
 
 func TestMap(t *testing.T) {
-  s := map[string] string {
-    "I like": "cake",
-    "And also": "ice cream",
-  }
-  expected := `
+	s := map[string] string {
+		"I like": "cake",
+		"And also": "ice cream",
+	}
+	expected := `
 map {
   "I like": "cake",
   "And also": "ice cream",
 }
-  `
-  assertOutput(t, s, expected)
+	`
+	assertOutput(t, s, expected)
 }
 
 func TestPointerMap(t *testing.T) {
-  s := map[string] string { "I like": "cake" }
-  expected := `
+	s := map[string] string { "I like": "cake" }
+	expected := `
 map {
   "I like": "cake",
 }
-  `
-  assertOutput(t, &s, expected)
+	`
+	assertOutput(t, &s, expected)
 }
 
 func TestMapOfMaps(t *testing.T) {
-  s := map[string] map[int]string {
-    "meh": map[int]string {9: "mergh"},
-    "tired of": map[int]string {123: "thinking of test cases"},
-  }
-  expected := `
+	s := map[string] map[int]string {
+		"meh": map[int]string {9: "mergh"},
+		"tired of": map[int]string {123: "thinking of test cases"},
+	}
+	expected := `
 map {
   "meh": map {
     9: "mergh",
@@ -195,16 +195,16 @@ map {
     123: "thinking of test cases",
   },
 }
-  `
-  assertOutput(t, s, expected)
+	`
+	assertOutput(t, s, expected)
 }
 
 func TestMapWithStruct(t *testing.T) {
-  s := map[int] Thing {
-    982: Thing {"nwle22"},
-    892: Thing {"ekel2n2l"},
-  }
-  expected := `
+	s := map[int] Thing {
+		982: Thing {"nwle22"},
+		892: Thing {"ekel2n2l"},
+	}
+	expected := `
 map {
   982: Thing {
     ThingValue: "nwle22"
@@ -213,6 +213,6 @@ map {
     ThingValue: "ekel2n2l"
   },
 }
-  `
-  assertOutput(t, s, expected)
+	`
+	assertOutput(t, s, expected)
 }
